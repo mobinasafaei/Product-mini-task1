1c14260efdd85c3a5bd257cf25a957010d2dc4b7
unknown <safaeimobina1381@gmail.com>
2023-12-25 23:01:34 +0330

    feat: infinite scrolling and pagination

--- pages/products.vue
+++ pages/products.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="ml-3 mr-3">
-    <div v-if="loading" class="flex justify-center">
-      <LoadingComp></LoadingComp>
-    </div>
-    <div v-else class="grid gap-3 grid-cols-4">
+    <div class="grid gap-3 grid-cols-4">
       <div v-for="(product,index) in products" :key="index">
         <ProductCardComp :product="product" :key="index"></ProductCardComp>
       </div>
+    </div>
+    <div v-if="loading" class="flex justify-center">
+      <LoadingComp></LoadingComp>
     </div>
   </div>
 </template>
@@ -14,18 +14,49 @@
 <script setup>
 const products = ref([]);
 const loading = ref(true);
+const totalPages = ref();
+const currentPageNumber = ref(1);
+
 onMounted(async () => {
-  products.value = await getProducts();
+  products.value = await getProducts(1);
+  scroll();
 });
-async function getProducts() {
+
+async function getProducts(pageNumber) {
   try {
     const response = await $fetch("/api/products", {
-      method: "GET"
+      method: "GET",
+      query: {
+        pageNumber: pageNumber
+      }
     });
     loading.value = false;
+    totalPages.value = response.meta.total_pages;
     return response.data;
   } catch (error) {
     console.log(error);
+  }
+}
+
+async function scroll() {
+  if (process.browser) {
+    const fetchingData = ref(false);
+    window.onscroll = async () => {
+      let bottomOfWindow =
+        document.documentElement.scrollTop + window.innerHeight ===
+        document.documentElement.offsetHeight;
+      if (bottomOfWindow) {
+        if (currentPageNumber.value < totalPages.value && !fetchingData.value) {
+          fetchingData.value = true;
+          const data = await getProducts((currentPageNumber.value += 1));
+          console.log(currentPageNumber.value);
+          data.map(x => {
+            products.value.push(x);
+          });
+          fetchingData.value = false;
+        }
+      }
+    };
   }
 }
 </script>
--- server/api/products.get.js
+++ server/api/products.get.js
@@ -1,7 +1,9 @@
 export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+    const pageNumber = query.pageNumber;
     const { public: { apiBase } } = useRuntimeConfig()
     try {
-        const products = await $fetch(`${apiBase}/products`, {
+        const products = await $fetch(`${apiBase}/products?page=${query.pageNumber}`, {
             method: "GET",
         })
         return products;
